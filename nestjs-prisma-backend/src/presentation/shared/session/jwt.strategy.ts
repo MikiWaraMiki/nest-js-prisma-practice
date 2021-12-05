e6364c781f634a72b9d7223ce6906578940cda07
TokorogaDokkoi <46447946+MikiWaraMiki@users.noreply.github.com>
2021-12-05 20:38:31 +0900

    wip api

--- nestjs-prisma-backend/src/presentation/shared/session/jwt.strategy.ts
+++ nestjs-prisma-backend/src/presentation/shared/session/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { passportJwtSecret } from "jwks-rsa";
 import { Starategy, ExtractJwt } from 'passport-jwt'
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Starategy) {
   constructor() {
